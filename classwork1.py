10f62bcd68330452b890b69bd213b495e79a5d60
Vivek <vivekjoseph28@gmail.com>
2025-12-30 15:37:18 +0530

    totalvolume(classwork-1)

---
+++ classwork1.py
@@ -0,0 +1,11 @@
+import random
+apple=15.5
+orange=20
+grape=10.25
+total_vol=apple+orange+grape
+print("Total volume=",total_vol)
+print("Volume in integer=",int(total_vol))
+print("this is total in string",str(total_vol))
+bonus=random.randrange(5, 10)
+final_total=total_vol+bonus
+print("Final Total",final_total)
